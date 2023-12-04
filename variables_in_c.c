ea1df8ebd4fa557ec413712a977e7b6e4ce05888
Hexia Valten <88661749+AnaisRevellat@users.noreply.github.com>
2023-12-04 19:31:03 +0100

    start

--- variables_in_c.c
+++ variables_in_c.c
@@ -3,7 +3,8 @@
 
 int main()
 {
-    printf("Hello World");
+    int user_age = 25, user_height = 175;
+    printf("Bonjour vous avez %d ans et vous mesurez %d cm", user_age, user_height);
 
     return 0;
 }
